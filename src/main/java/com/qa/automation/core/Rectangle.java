8df65b3df0bdaa3ee399f685ea5eca092a4e9543
Balbinder Sanjeevani <balbinder.s93@gmail.com>
2025-09-18 16:33:51 +0530

    feat: add Shape hierarchy (Circle, Rectangle) with OOP basics and tests (Week1 Day3)

---
+++ src/main/java/com/qa/automation/core/Rectangle.java
@@ -0,0 +1,31 @@
+package com.qa.automation.core;
+
+public class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(String color, double length, double width) {
+        super(color);
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+}
+
